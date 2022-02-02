4c27568d162d4af3bf53f84c5d29434df9afda10
Leonardo <leo43161301@gmail.com>
2022-02-02 08:23:03 -0300

    Se cambio el url del back por el que está en remoto en heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-const URLServerLocal = "http://localhost:5000/";
+/* const URLServerLocal = "http://localhost:5000/"; */
 const URLPageLocal = "http://localhost:3000/";
-/* const URLServerHost = "https://bar-code-1.herokuapp.com/";
-const URLPageHost = "https://bar-code.netlify.app/"; */
-export const URLServer = URLServerLocal;
+const URLServerHost = "https://bar-code-1.herokuapp.com/";
+/* const URLPageHost = "https://bar-code.netlify.app/"; */
+export const URLServer = URLServerHost;
 export const BAR = "barcode";
 export const URLPage = URLPageLocal;
 export const URLImg = "https://img-barcode.s3.sa-east-1.amazonaws.com/";
