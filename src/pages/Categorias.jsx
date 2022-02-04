17f13eae24b203d88613d3f4c7f6cc41d801b92b
Leonardo <leo43161301@gmail.com>
2022-02-04 14:51:54 -0300

    Se agregó pequeños detalle

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -5,7 +5,7 @@ import useCategorias from '../hooks/useCategorias';
 export default function Categorias() {
     const { categorias } = useCategorias();
     return (
-        <div className="w-100 d-flex justify-content-center">
+        <div className="w-100 d-flex justify-content-center mb-4">
             <div className="g-4 mt-1 row row-cols-md-3 row-cols-2 w-100">
                 {categorias.map((value, idx) => (
                     <div key={idx}>
